8b8bd908a5054fa65f17ab2fc170d3f9c247cd60
KamisettyLokesh <lokesh6167@gmail.com>
2023-10-24 19:21:51 +0530

    Refresh issue fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 
 function App() {
+  
   return (
     <ChatEngine
       projectID='2dec56db-af5f-4e84-af94-ee0da1332fbe'
       userName='Lokesh'
       userSecret='lokesh@123'
+      height='calc(100vh)'
     />
   );
 }
--- src/DirectChatPage.js
+++ src/DirectChatPage.js
@@ -1,42 +1,104 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { ChatEngine, getOrCreateChat, getChats } from 'react-chat-engine';
 
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+const ChatPage = () => {
+  const [username, setUsername] = useState('');
+  const [chatID, setChatID] = useState('');
+  const [participants, setParticipants] = useState([]);
+  const [isDirectChat, setIsDirectChat] = useState(true);
+  const [chats, setChats] = useState([]);
 
-const DirectChatPage = () => {
-    const [username, setUsername] = useState('')
+  const creds = {
+    projectID: "2dec56db-af5f-4e84-af94-ee0da1332fbe",
+    userName: "Lokesh",
+    userSecret: "lokesh@123"
+  }
 
-    function createDirectChat(creds) {
-        getOrCreateChat(
-            creds,
-            { is_direct_chat: true, usernames: [username] },
-            () => setUsername('')
-        )
+  useEffect(() => {
+    const interval = setInterval(() => {
+      syncChatList(creds);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const syncChatList = async (creds) => {
+    const chatList = await getChats(creds);
+    setChats(chatList);
+  };
+
+  const refreshWebPage = () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000)
+  }
+  const createChat = (creds) => {
+    if (isDirectChat) {
+      // Create a direct chat
+      getOrCreateChat(
+        creds,
+        { is_direct_chat: true, usernames: [username] },
+        () => setUsername('')
+      );
+    } else {
+      // Create a group chat with chatID and participants
+      getOrCreateChat(
+        creds,
+        { is_direct_chat: false, title: chatID, usernames: participants },
+        () => {
+          setChatID('');
+          setParticipants([]);
+        }
+      );
     }
+    refreshWebPage();
+  };
 
-    function renderChatForm(creds) {
-        return (
-            <div>
-                <input
-                    placeholder='Username'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <button onClick={() => createDirectChat(creds)}>
-                    Create
-                </button>
-            </div>
-        )
-    }
-
+  const renderChatForm = (creds) => {
     return (
-        <ChatEngine
-            height='100vh'
-            projectID='2dec56db-af5f-4e84-af94-ee0da1332fbe'
-            userName='Lokesh'
-            userSecret='lokesh@123'
-            renderNewChatForm={(creds) => renderChatForm(creds)}
-        />
-    )
-}
+      <div>
+        {isDirectChat ? (
+          <div>
+            <input
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button onClick={() => createChat(creds)}>Create Direct Chat</button>
+          </div>
+        ) : (
+          <div>
+            <input
+              placeholder="Group Name (Chat ID)"
+              value={chatID}
+              onChange={(e) => setChatID(e.target.value)}
+            />
+            <input
+              placeholder="Usernames (comma-separated)"
+              value={participants}
+              onChange={(e) => setParticipants(e.target.value.split(','))}
+            />
+            <button onClick={() => createChat(creds)}>Create Group Chat</button>
+          </div>
+        )}
 
-export default DirectChatPage;
+        <div>
+          <button onClick={() => setIsDirectChat(!isDirectChat)}>
+            Toggle Chat Type
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="2dec56db-af5f-4e84-af94-ee0da1332fbe"
+      userName="Lokesh"
+      userSecret="lokesh@123"
+      renderNewChatForm={(creds) => renderChatForm(creds)}
+    />
+  );
+};
+
+export default ChatPage;
